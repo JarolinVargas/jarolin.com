3c0c0273ec8b563882a48dccebfad3c50d9d8aa3
Jarolin Vargas <jarolinvargas@gmail.com>
2020-04-11 23:08:43 -0400

    layout basics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li className="nav-separator"></li>
+          <li><a href="#">ABOUT ME</a></li>
+          <li className="nav-separator"></li>
+          <li><a href="#">PROJECTS</a></li>
+          <li className="nav-separator"></li>
+          <li><a href="#">THOUGHTS</a></li>
+          <li className="nav-separator"></li>
+        </ul>
+      </nav>
+      <div className="frame"></div>
+      <div className="content">
+
+      </div>
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '.components/Nav.scss';
+
+export default function Nav() {
+    return (
+        <nav>
+
+        </nav>
+    )
+}
