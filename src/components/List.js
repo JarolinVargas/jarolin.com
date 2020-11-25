ca65fdadefcb2e6d5ac6300a67f5eac0c010008f
jarolin vargas <jarolinvargas@jarolins-MacBook-Pro.local>
2020-11-24 22:49:32 -0500

    meta details

--- src/components/List.js
+++ src/components/List.js
@@ -1,45 +1,20 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { motion } from "framer";
 import './List.scss';
-
-const animations = {
-    initial: {
-        y: -50,
-        display: 'none',
-        opacity: 0
-    },
-    enter: {
-        y: 0,
-        display: 'block',
-        opacity: 1,
-        transition: {
-            ease: "easeOut"
-        }
-    }
-}
-
-const staggerAnimation = {
-	enter: {
-        transition: {
-            staggerChildren: 0.1
-        }
-    }
-}
 
 export default function List(props) {
     let listItems;
     if( props.portfolioList ) {
         listItems = props.portfolioList.map((item, i) =>
-            <motion.li key={i} variants={animations}>
+            <li key={i}>
                 <Link to={`portfolio/${item.sys['id']}`}>{item.projectTitle}</Link>
-            </motion.li>
+            </li>
         )
     }
 
     return (
-        <motion.ul className="List" initial="initial" animate="enter" variants={staggerAnimation}>
+        <ul className="List">
             {listItems}
-        </motion.ul>
+        </ul>
     )
 }
