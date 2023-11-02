74e90b2ab6e63189e0ff2926381eeb784c1e443b
JarolinVargas <jarolinvargas@gmail.com>
2023-11-01 20:20:08 -0400

    resume link fix

--- src/components/IconLinks.js
+++ src/components/IconLinks.js
@@ -27,10 +27,10 @@ export default function IconLinks() {
     return (
         <div className="IconLinks">
             <motion.ul variants={animations}>
-                <li><a href="https://github.com/JarolinVargas" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} color="#FFF"/></a></li>
-                <li><a href="https://dribbble.com/JarolinVargas" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faDribbble} color="#EA4C89"/></a></li>
-                <li><a href="https://www.linkedin.com/in/jarolin-vargas-04099b156/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} color="#028fc7"/></a></li>
-                <li><a href="https://assets.ctfassets.net/0mo16etbpbdn/6jgB3Wr6gJq4KzxCLs5qBv/394106a9194320daf446e716297ffa3c/5666456-2f5f8f0e069784b9b97c6ca9880bc5ba.pdf" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFileAlt} color="orange"/></a></li>
+                <li><a href="https://github.com/JarolinVargas" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} color="#FFF" /></a></li>
+                <li><a href="https://dribbble.com/JarolinVargas" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faDribbble} color="#EA4C89" /></a></li>
+                <li><a href="https://www.linkedin.com/in/jarolin-vargas-04099b156/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} color="#028fc7" /></a></li>
+                <li><a href="resume.pdf" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFileAlt} color="orange" /></a></li>
             </motion.ul>
         </div>
     )
